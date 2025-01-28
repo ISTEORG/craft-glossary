9c5b6602a93a8561f9bdd677c3b347af61c43d99
Grant McClure <grant.mcclure@t4bi.com>
2025-01-27 16:19:55 -0800

    Resolve Twig 3.15 deprecation issue #4

--- src/services/Terms.php
+++ src/services/Terms.php
@@ -72,7 +72,7 @@ class Terms extends Component
                     }
                     $text = s($text)->replaceMatches($pattern, function ($matches) use ($term, &$replacements, &$index, $view, $glossary) {
                         try {
-                            $replacement = trim($view->c('glossary/view/term', [
+                            $replacement = trim($view->renderTemplate('glossary/view/term', [
                                 'term' => $term,
                                 'text' => $matches[0],
                             ], 'site'));
